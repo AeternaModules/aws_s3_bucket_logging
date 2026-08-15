bba1a997973823e04cf601fa41e11d07921dfcca
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:46:23 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -24,7 +24,7 @@ output "s3_bucket_loggings_target_grant" {
 }
 output "s3_bucket_loggings_target_object_key_format" {
   description = "Map of target_object_key_format values across all s3_bucket_loggings, keyed the same as var.s3_bucket_loggings"
-  value       = { for k, v in aws_s3_bucket_logging.s3_bucket_loggings : k => v.target_object_key_format if v.target_object_key_format != null && length(v.target_object_key_format) > 0 }
+  value       = { for k, v in aws_s3_bucket_logging.s3_bucket_loggings : k => one(v.target_object_key_format) if v.target_object_key_format != null && length(v.target_object_key_format) > 0 }
 }
 output "s3_bucket_loggings_target_prefix" {
   description = "Map of target_prefix values across all s3_bucket_loggings, keyed the same as var.s3_bucket_loggings"
